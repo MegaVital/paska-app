07831d49d8200a36753122c2b9f9dda460ffd5e2
MegaVital <kalist.fl@gmail.com>
2023-04-18 15:43:23 +0300

    Updates

---
+++ precache-manifest.e8538c871a9624232b79f287a4f4af29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f443af15b5ae462fcf",
+    "url": "/Furniture-Shop-app/static/css/main.3d1ffed7.chunk.css"
+  },
+  {
+    "revision": "c8f443af15b5ae462fcf",
+    "url": "/Furniture-Shop-app/static/js/main.c8f443af.chunk.js"
+  },
+  {
+    "revision": "43ecc3edd36cf7e322b5",
+    "url": "/Furniture-Shop-app/static/js/1.43ecc3ed.chunk.js"
+  },
+  {
+    "revision": "9d3f8afc713c8f6e7c13",
+    "url": "/Furniture-Shop-app/static/css/2.2db457ef.chunk.css"
+  },
+  {
+    "revision": "9d3f8afc713c8f6e7c13",
+    "url": "/Furniture-Shop-app/static/js/2.9d3f8afc.chunk.js"
+  },
+  {
+    "revision": "3d953415951b873089bc",
+    "url": "/Furniture-Shop-app/static/js/runtime~main.3d953415.js"
+  },
+  {
+    "revision": "7822af1ee75a0769e057db29749f68fc",
+    "url": "/Furniture-Shop-app/index.html"
+  }
+];
